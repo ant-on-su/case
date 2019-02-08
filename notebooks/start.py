10cb1f810fc6418b2be5267fb9d25ad3e2ccad91
anto_on_su <a.sugonyako@gmail.com>
2019-02-08 17:03:24 +0100

    second notebook

--- notebooks/start.py
+++ notebooks/start.py
@@ -7,13 +7,17 @@ import numpy as np
 import pandas_profiling
 
 #%%
-df=pd.read_csv('./data/listings.csv')
+df=pd.read_csv('../data/listings.csv')
+df.head()
+
+#%%
+df['last_review']=pd.to_datetime(df['last_review'], yearfirst=True)
+df.dtypes
+
+#%%
+df['months_exist']=df['reviews_per_month']*df['number_of_reviews']
 df.head()
 
 #%%
 pandas_profiling.ProfileReport(df).to_file(
-outputfile='./output/pd-profiling.html')
-
-#%%
-pd.to_datetime(df['last_review'])
-df.dtypes
+outputfile='../output/pd-profiling.html')
